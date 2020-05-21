86461074970383578b1b93124be5da3c1518a2ef
RdtBui <richard.bui@member.mensa.org>
2020-05-21 18:15:59 -0400

    install and configure react-native-fs

--- android/app/src/main/java/com/fruitapp/MainApplication.java
+++ android/app/src/main/java/com/fruitapp/MainApplication.java
@@ -9,6 +9,7 @@ import org.unimodules.adapters.react.ReactModuleRegistryProvider;
 import org.unimodules.core.interfaces.SingletonModule;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
